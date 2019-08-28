862c5fbd888b87f8c8d647871c01bd72f281da96
huangshihao <750453128@qq.com>
2019-08-28 15:45:31 +0800

    新增数组洗牌排序、优雅处理async/await异常

--- common-utils.js
+++ common-utils.js
@@ -32,3 +32,28 @@ export const debounce = (func, time = 300, options = {
   }
   return _debounce;
 }
+/* 
+*  数组洗牌  随机打乱
+*  @arr 需要打乱的数组
+*  描述：原地洗牌算法，可以达到真正的乱序，原理是拿当前元素跟之后的所有元素中随机的一个交换位置；
+*/
+export function shuffle(arr) {
+  for(let i=0; i < arr.length; i++) {
+    let randomIndex = i + Math.floor(Math.random() * (arr.length - i));
+    [ arr[i], arr[randomIndex] ] = [ arr[randomIndex], arr[i] ];
+  }
+  return arr;
+}
+/* 
+*  处理 async/await 异常情况
+*  
+*  描述：示例：let [err, res] = errorCaptured( asyncFunc );
+*/
+export function errorCaptured(asyncFunc) {
+  try {
+    let res = await asyncFunc();
+    return [null, res];
+  } catch (e) {
+    return [e, null];
+  }
+}
